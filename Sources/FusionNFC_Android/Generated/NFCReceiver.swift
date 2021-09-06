4e289694752cd4ff1214dc16e4be56e05fe693a3
Pavlo Boiko <boiko.impul@gmail.com>
2021-09-06 09:06:26 +0300

    Update NFCReceiver

---
+++ Sources/FusionNFC_Android/Generated/NFCReceiver.swift
@@ -0,0 +1,16 @@
+#if os(Android)
+
+import Java
+import Android
+import AndroidOS
+import AndroidApp
+import AndroidContent
+import AndroidNFC
+
+@_silgen_name("Java_com_FusionNFC_1Android_NFCReceiver_onReceiveImpl")
+public func NFCReceiver_onReceiveImpl(env: UnsafeMutablePointer<JNIEnv>, obj: JavaObject?, ptr: JavaLong, context: JavaObject?, intent: JavaObject?) -> Void {
+	let _context = cast(Object?.fromJavaObject(context), to: ContextProxy.self)	let _intent = Intent?.fromJavaObject(intent)
+		NFCReceiver.didReceive(context: _context, intent: _intent)
+}
+
+#endif
